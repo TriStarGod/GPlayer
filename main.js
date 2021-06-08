07f1a179956edbc301ff84d52d6c065feb35f0a5
Dinesh A <dfa2222@gmail.com>
2021-06-08 11:40:13 -0400

    added wav

--- main.js
+++ main.js
@@ -7,66 +7,167 @@
 document.addEventListener("DOMContentLoaded", function (g) {
 	window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
-	function CompleteClose(event, playerInstance, container, audioArray) {
-		event.stopPropagation ? event.stopPropagation() : (event.cancelBubble = true);
-		if (playerInstance.active() != true)
-			return;
-		playerInstance.close();
-		playerInstance = null;
-		if (audioArray) audioArray = null;
-		container.parentNode.removeChild(container);
-		container = null;
+	function CompleteClose(e, playerInstance, container) {
+		if (e) e.stopPropagation ? e.stopPropagation() : (e.cancelBubble = true);
+		if (container) container.parentNode.removeChild(container);
+		if (playerInstance && playerInstance.active()) playerInstance.close();
+		// playerInstance = null;
+		// container = null;
 	}
 
-	function LoadFile(url, overlay, container, location, ldmsg) {
-		const request = new XMLHttpRequest();
-		request.open('GET', url, true);
-		request.responseType = 'arraybuffer';
-		request.onload = function () {
-			const { readyState, status, response } = this;
-			// console.log(`readyState: ${readyState} - status: ${status}`);
-			if (readyState === 4 && status === 200) {
-				ldmsg.innerHTML = "";
-				const playerElement = InsertPlayer(location);
-				window.player = new Player(playerElement, response);
-				overlay.addEventListener("click", function (e) { CompleteClose(e, window.player, container); });
-			}
+	// can play mp3s and wav
+	function DecodeAudio(files) {
+		if (files.length === 0) return;
+		const slicedList = Array.prototype.slice.call(files);
+		for (let i = 0; i < slicedList.length; i++) {
+			const input = slicedList[i];
+			input.classList.add("hasMP3Player");
+			let url = input.getAttribute("download_url");
+			url = url.substring(url.search(":http") + 1);
+			input.addEventListener('click', (e) => {
+				e.preventDefault();
+				e.stopPropagation();
+				// e.stopImmediatePropagation();
+				// e.stopPropagation ? e.stopPropagation() : (e.cancelBubble=true);
+				const request = new XMLHttpRequest();
+				request.open('GET', url, true);
+				request.responseType = 'arraybuffer';
+				request.onload = function () {
+					let container;
+					let overlay;
+					let playerInstance;
+					let ldmsg;
+					try {
+						container = document.createDocumentFragment().appendChild(document.createElement("div"));
+						container.classList.add("playerDisplay");
+						overlay = container.appendChild(document.createElement("div"));
+						overlay.classList.add("overlay");
+						const location = container.appendChild(document.createElement("div"));
+						location.classList.add("overlayPlayer");
+						ldmsg = location.appendChild(document.createElement("p"));
+						ldmsg.classList.add("initLoader");
+						document.body.appendChild(container);
+						ldmsg.innerHTML = "Loading file";
+
+						const { readyState, status, response } = this;
+						// console.log(`readyState: ${readyState} - status: ${status}`);
+						if (readyState === 4 && status === 200) {
+							ldmsg.innerHTML = "";
+							const playerElement = InsertPlayer(location);
+							playerInstance = window.player = new Player(playerElement, response);
+						}
+					} catch (error) {
+						console.error("GPlayer failed to load file", error)
+						if (ldmsg) ldmsg.innerHTML = "GPlayer failed to load file";
+					} finally {
+						overlay.addEventListener("click", (e) => CompleteClose(e, playerInstance, container));
+					}
+				}
+				request.send();
+			}, false);
 		}
-		request.send();
 	}
+	// NOT working
+	function CustomDecodeWAV(files) {
+		if (files.length === 0) return;
+		const slicedList = Array.prototype.slice.call(files);
+		for (let i = 0; i < slicedList.length; i++) {
+			const input = slicedList[i];
+			input.classList.add("hasMP3Player");
+			let url = input.getAttribute("download_url");
+			url = url.substring(url.search(":http") + 1);
+			input.addEventListener('click', (e) => {
+				e.preventDefault();
+				e.stopPropagation();
+				// e.stopImmediatePropagation();
+				// e.stopPropagation ? e.stopPropagation() : (e.cancelBubble=true);
+				const request = new XMLHttpRequest();
+				request.open('GET', url, true);
+				request.responseType = 'arraybuffer';
+				request.onload = function () {
+					let container;
+					let overlay;
+					let playerInstance;
+					let ldmsg;
+					try {
+						container = document.createDocumentFragment().appendChild(document.createElement("div"));
+						container.classList.add("playerDisplay");
+						overlay = container.appendChild(document.createElement("div"));
+						overlay.classList.add("overlay");
+						const location = container.appendChild(document.createElement("div"));
+						location.classList.add("overlayPlayer");
+						ldmsg = location.appendChild(document.createElement("p"));
+						ldmsg.classList.add("initLoader");
+						document.body.appendChild(container);
+						ldmsg.innerHTML = "Loading file";
 
-	function OverlaySetup(event, dlURL, aPass) {
-		event.preventDefault();
-		event.stopPropagation();
-		// event.stopImmediatePropagation();
-		// event.stopPropagation ? event.stopPropagation() : (event.cancelBubble=true);
-		const holder = document.createDocumentFragment().appendChild(document.createElement("div"));
-		holder.classList.add("playerDisplay");
-		const oB = holder.appendChild(document.createElement("div"));
-		oB.classList.add("overlay");
-		const oCont = holder.appendChild(document.createElement("div"));
-		oCont.classList.add("overlayPlayer");
-		const p0 = oCont.appendChild(document.createElement("p"));
-		p0.classList.add("initLoader");
-		document.body.appendChild(holder);
-		p0.innerHTML = "Loading mp3";
-		LoadFile(dlURL, oB, holder, oCont, p0);
-	}
+						const { readyState, status, response } = this;
+						// console.log(`readyState: ${readyState} - status: ${status}`);
+						if (readyState === 4 && status === 200) {
+							ldmsg.innerHTML = "";
+							const playerElement = InsertPlayer(location);
 
-	function Mp3sDecode(e) {
-		const mp3List = document.querySelectorAll("*[download_url*='.mp3']:not(.hasMP3Player)");
-		if (mp3List.length !== 0) {
-			const slicedMP3List = Array.prototype.slice.call(mp3List);
-			for (let i = 0; i < slicedMP3List.length; i++) {
-				const input = slicedMP3List[i];
-				input.classList.add("hasMP3Player");
-				let dlURL = input.getAttribute("download_url");
-				dlURL = dlURL.substring(dlURL.search(":http") + 1);
-				input.addEventListener('click', (e) => OverlaySetup(e, dlURL), false);
-			}
+							let audioData = response;
+							let dv = new DataView(audioData);
+							let junk = 0;
+							let position = 12;
+							do {
+								const header = String.fromCharCode.apply(null, new Uint8Array(audioData, position, 4));
+								const length = dv.getUint32(position + 4, true);
+								if (header.trim() === 'fmt') {
+									junk = junk + length - 16;
+								}
+								position = position + 8 + length;
+							} while (position < audioData.byteLength);
+							const productArray = new Uint8Array(audioData.byteLength - junk);
+							productArray.set(new Uint8Array(audioData, 0, 12));
+							let newPosition = 12;
+							position = 12;
+							let fmt_length_spot = 0;
+							do {
+								const header = String.fromCharCode.apply(null, new Uint8Array(audioData, position, 4));
+								const length = dv.getUint32(position + 4, true);
+								if (header.trim() === 'fmt') {
+									productArray.set(new Uint8Array(audioData, position, 24), newPosition);
+									fmt_length_spot = newPosition + 4;
+									newPosition = newPosition + 24;
+								}
+								else {
+									productArray.set(new Uint8Array(audioData, position, length + 8), newPosition);
+									newPosition = newPosition + 8 + length;
+								}
+								position = position + 8 + length;
+							} while (position < audioData.byteLength);
+							audioData = productArray.buffer;
+							dv = new DataView(audioData);
+							dv.setUint32(4, audioData.byteLength - 8, true);
+							dv.setUint32(fmt_length_spot, 16, true);
+
+							playerInstance = window.player = new Player(playerElement, response);
+						}
+					} catch (error) {
+						console.error("GPlayer failed to load file", error)
+						if (ldmsg) ldmsg.innerHTML = "GPlayer failed to load file";
+					} finally {
+						overlay.addEventListener("click", (e) => CompleteClose(e, playerInstance, container));
+					}
+				}
+				request.send();
+			}, false);
 		}
+	}
+
+	function Activate(e) {
+		let files = document.querySelectorAll("*[download_url*='.mp3']:not(.hasMP3Player)");
+		DecodeAudio(files);
+		files = document.querySelectorAll("*[download_url*='.MP3']:not(.hasMP3Player)");
+		DecodeAudio(files);
+		files = document.querySelectorAll("*[download_url*='.wav']:not(.hasMP3Player)");
+		DecodeAudio(files);
+		files = document.querySelectorAll("*[download_url*='.WAV']:not(.hasMP3Player)");
+		DecodeAudio(files);
 	};
 
-	document.addEventListener("DOMNodeInserted", Mp3sDecode.bind(this));
+	document.addEventListener("DOMNodeInserted", Activate.bind(this));
 
 });
